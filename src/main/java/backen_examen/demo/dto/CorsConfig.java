49f263f9063feee6610b6f986043f32a549115e2
Antonio Mata Gil de Rozas <anto.mata1@gmail.com>
2026-05-09 22:23:28 +0200

    add new url on cors

--- src/main/java/backen_examen/demo/dto/CorsConfig.java
+++ src/main/java/backen_examen/demo/dto/CorsConfig.java
@@ -18,7 +18,8 @@ public class CorsConfig implements WebMvcConfigurer {
                 "http://localhost:3000",
                 "http://localhost:8080", 
                 "http://127.0.0.1:3000",
-                "http://127.0.0.1:8080"
+                "http://127.0.0.1:8080",
+                "https://frontend-react-vite-dam-pdauf.vercel.app"
             )
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
